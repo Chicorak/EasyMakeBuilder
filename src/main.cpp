73bdcfd0bbc00950863a4d449c27e7ee068d8750
Poison <35471036+coolcoy12@users.noreply.github.com>
2020-12-01 19:15:24 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,23 @@
 #include "string"
 #include "fstream"
-#include "iomanip"
 #include "iostream"
 #include <termcolor.hpp>
-#include <json.hpp>
 
-using json = nlohmann::json;
 
 int main() 
-{
+{   
+   
     std::string ProjectName;
     std::string Compiler;
     std::string Output;
     std::string Source;
     std::string Include;
     std::string CompilerOptions; 
-    nlohmann::json j;
 
-    std::cout << termcolor::blue << termcolor::bold << "EasyMakeBuilder" << std::endl << termcolor::italic << "//Made By CodePoison;" << std::endl << std::endl << std::endl;
+    std::cout << termcolor::blue << termcolor::bold << "EasyMakeBuilder" << std::endl  << "//Made By CodePoison;" << std::endl << std::endl << std::endl;
     
     
-    std::cout << termcolor::green << "Name of Project: ";
+    std::cout << termcolor::reset << termcolor::green << "Name of Project: ";
     std::cin >> ProjectName; 
     
     std::cout << termcolor::green << "Compiler: ";
@@ -38,14 +35,22 @@ int main()
     std::cout << termcolor::green << "Extra Compiler Options ( sperated by space ): ";    
     std::cin >> CompilerOptions;
 
-
-    j["project"] = ProjectName;
-    j["compiler"] = Compiler;
-    j["output"] = Output;
-    j["sources"] = Source;
-    j["include"] = Include;
-    j["compiler_options"] = CompilerOptions;
-
     std::ofstream o("autobuild.ezmk");
-    o << std::setw(4) << j << std::endl;
+    o << "{" << std::endl;
+    o << "  \"Project\":\"" << ProjectName << "\"," << std::endl;
+    o << "  \"compiler\":\"" << Compiler << "\"," << std::endl;
+    o << "  \"output\":\"" << Output << "\"," << std::endl;
+    o << std::endl;
+    o << "  \"includes\": [" << std::endl;
+    o << "      \"" << Include << "\"" << std::endl; 
+    o << "  ],";
+    o << std::endl;
+    o << "  \"sources\": [" << std::endl;
+    o << "    \"" << Source << "\"" << std::endl; 
+    o << "  ],";
+    o << std::endl;
+    o << "  \"compiler_options\": [" << std::endl;
+    o << "    \"" << CompilerOptions << "\"" << std::endl; 
+    o << "  ],";
+    o << "}" << std::endl;
 }
